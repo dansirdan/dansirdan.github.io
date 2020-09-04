3764be2366ffd2cce7363fae7f4c40b64e601b8c
dansirdan <daniel.monteton@gmail.com>
2020-09-04 16:28:41 -0700

    troubleshooting gh-pages

---
+++ build/precache-manifest.4532b52c2fcae030358e8d943d4dc261.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe7805a24f1a02150dae0ab33a5600e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66e5a2e4b30d8fcc1958",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1741bb2e85034ab26e5a",
+    "url": "/static/js/2.d7435022.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7435022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66e5a2e4b30d8fcc1958",
+    "url": "/static/js/main.b67748ba.chunk.js"
+  },
+  {
+    "revision": "6c4ba42b22b58f472ee1",
+    "url": "/static/js/runtime-main.ef53b05d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
