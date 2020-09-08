12bda0ca7a65ba30e5617e3f9c6f77b33529ad9c
dansirdan <daniel.monteton@gmail.com>
2020-09-08 15:40:17 -0700

    ran deploy for gh-pages

---
+++ build/precache-manifest.3fb372af29596b73de5a3f5ba462c1f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ccf9069da303b7e1d28e38fde801ec1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ece9bcb0fd482798f504",
+    "url": "/static/css/2.53159467.chunk.css"
+  },
+  {
+    "revision": "c6824cf8b75462c7861d",
+    "url": "/static/css/main.dc21bb33.chunk.css"
+  },
+  {
+    "revision": "ece9bcb0fd482798f504",
+    "url": "/static/js/2.2d9f7044.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.2d9f7044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6824cf8b75462c7861d",
+    "url": "/static/js/main.038becac.chunk.js"
+  },
+  {
+    "revision": "880a1b9875f2941d47ba",
+    "url": "/static/js/runtime-main.1d249076.js"
+  }
+]);
